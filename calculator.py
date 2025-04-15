a039fa123cd511ca1b804aa3021b9d414bbd4951
Aryaan Verma <aryaanverma@Aryaans-MacBook-Pro.local>
2025-04-14 22:58:59 -0400

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -4,8 +4,23 @@ calculator.py
 
 One function per operation, in order.
 """
+import math
 # First example
 def add(a, b): 
-    pass
+    return a + b
 
+def sub(a,b):
+    return a - b
+
+def mul(a, b): 
+    return a * b
+
+def div(a, b): 
+    return a / b
+
+def log(a, b): 
+    return math.log(b, a)
+
+def exp(a, b): 
+    return a ** b
 
